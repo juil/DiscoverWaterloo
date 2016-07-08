b64c4d7745f6b7bf3057b5f5090354c8deb1da98
Juil Yoon <jiy.juil@gmail.com>
2016-07-08 12:25:50 -0400

    Add location class

---
+++ app/src/main/java/com/juilyoon/discoverwaterloo/Location.java
@@ -0,0 +1,50 @@
+package com.juilyoon.discoverwaterloo;
+
+/**
+ * Created by juil on 16-07-08.
+ */
+public class Location {
+    /** The name of the location. */
+    private String name;
+    /** A integer rating 1-5 */
+    private int rating;
+    /** Short description of location. */
+    private String description;
+    /** Link to location in review site. */
+    private String reviewUrl;
+    /** Drawable id of representative image. */
+    private int imageResourceId;
+
+    public Location(String name, int rating, String description, String reviewUrl, int imageResourceId) {
+        this.name = name;
+        this.rating = rating;
+        this.description = description;
+        this.reviewUrl = reviewUrl;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+
+        return description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getReviewUrl() {
+        return reviewUrl;
+    }
+
+    public String getReviewUrl_formatted() {
+        return "&lt;a href=\"" + reviewUrl +"\">" + name + "&lt;/a>";
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+}
