4c91cbc014dc3b106d2897c58c6efa74a0758056
Juil Yoon <jiy.juil@gmail.com>
2016-07-11 18:49:48 -0400

    Remove menu

--- app/src/main/java/com/juilyoon/discoverwaterloo/MainActivity.java
+++ app/src/main/java/com/juilyoon/discoverwaterloo/MainActivity.java
@@ -60,14 +60,6 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(mViewPager);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
